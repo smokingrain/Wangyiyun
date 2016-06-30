fb505cd85f7b2c40e64fe61a7c0c84dc18f41df6
PDD <1575455507@qq.com>
2016-07-01 00:09:33 +0800

    logo button

--- components/Header.js
+++ components/Header.js
@@ -9,8 +9,18 @@ export default class Header extends Component {
     return (
       <div className="header">
         <div className="left">
-          <div className="logo"></div>
-          <div className="record"></div>
+          <div className="logo">
+            <div className="author">
+              <p className="authorname">PDD</p>
+              <p className="boxname">魔法音乐盒</p>
+            </div>
+          </div>
+          <div className="record">
+            <div className="buttonbox">
+              <span className="glyphicon glyphicon-menu-left"></span>
+              <span className="glyphicon glyphicon-menu-right disabled"></span>
+            </div>
+          </div>
           <div classNam="search"></div>
         </div>
         <div className="right"></div>
